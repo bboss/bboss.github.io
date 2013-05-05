dbf369aa6cf8655a789ab6cac2c6645b1b782b6f
bboss <mail.bproject@gmail.com>
2013-05-05 21:54:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Bboss @ bproject</title>
 </head>
 <body>
-<h3>Welcome to my page(the page of Bboss ;)</h3>
+<h3>Welcome on my page(the page of Bboss ;)</h3>
 <p><b>This site is under construction now!<b> More content will comming soon!</p>
 </body>
 </html>
