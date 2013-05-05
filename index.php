1ada33df54cea9cd73aeb452563999128fafe064
bproject <info@bproject.hourb.com>
2013-05-05 20:49:34 +0200

    homepage

---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<head>
+<title>Bboss @ bproject</title>
+</head>
+<body>
+<h3>Welcome to my page(the page of Bboss ;)</h3>
+<p><b>This site is under construction now!<b> More content will comming soon!</p>
+</body>
+</html>
